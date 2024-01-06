736e260057b30adbce26002ab92016491a5a4ea7
Umberto194 <schiavoneumberto00@gmail.com>
2024-01-06 18:40:16 +0100

    add call api friends and posts

--- frontend/src/app/personal-page/personal-page.component.ts
+++ frontend/src/app/personal-page/personal-page.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../pages/confirmation-dialog/confirmation-dialog.component';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-personal-page',
   templateUrl: './personal-page.component.html',
   styleUrls: ['./personal-page.component.scss']
 })
-export class PersonalPageComponent {
-  friendsList: any[] = [
-    { id: 1, name: 'Amico 1' },
-    { id: 2, name: 'Amico 2' },
-    { id: 3, name: 'Amico 3' },
-    { id: 4, name: 'Amico 4' },
-    { id: 5, name: 'Amico 5' },
-    { id: 6, name: 'Amico 6' },
-  ];
+export class PersonalPageComponent implements OnInit {
+  friendsList: any[] = [];
+  postList: any[] = [];
 
-  postList: any[] = [
-    { content: 'Questo è un post.' },
-    { content: 'Un altro post interessante.' },
-    { content: 'Un terzo post.' },
-  ];
+  constructor(
+    private dialog: MatDialog,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
-  constructor(private dialog: MatDialog, private router: Router) { }
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/api/v1/friends')
+      .subscribe(
+        friends => {
+          this.friendsList = friends;
+        },
+        error => {
+          console.error('Errore nella chiamata API per ottenere la lista di amici:', error);
+        }
+      );
+
+    this.http.get<any[]>('http://localhost:3000/api/v1/posts')
+      .subscribe(
+        posts => {
+          this.postList = posts;
+        },
+        error => {
+          console.error('Errore nella chiamata API per ottenere la lista di post:', error);
+        }
+      );
+  }
 
   openConfirmationDialog(friend: any): void {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
