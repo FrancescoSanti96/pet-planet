2f21079d703cb0a89180f50e6ca1a18e6e9f4c17
Umberto194 <schiavoneumberto00@gmail.com>
2024-01-08 23:07:35 +0100

    add google logo

--- frontend/src/app/component/google-auth/google-auth.component.ts
+++ frontend/src/app/component/google-auth/google-auth.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+const googleLogoURL = "https://raw.githubusercontent.com/fireflysemantics/logo/master/Google.svg";
 
 @Component({
   selector: 'app-google-auth',
-  template: `
-    <a [href]="getGoogleAuthUrl()">
-      Google Login
-    </a>
-  `,
+  templateUrl: './google-auth.component.html',
+  styleUrl: './google-auth.component.scss'
 })
 export class GoogleAuthComponent {
-  constructor() {}
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "logo",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(googleLogoURL));
+  }
 
   getGoogleAuthUrl(): string {
     return 'http://localhost:3000/oauth2/google'; // Adjust the URL accordingly
