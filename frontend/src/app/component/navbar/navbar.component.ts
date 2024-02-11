9fc8b6da276a1c913730f66857c0e30e2a8c954e
FrancescoSanti96 <francescosanti123@gmail.com>
2024-02-11 15:18:20 +0100

    add img on navbar

--- frontend/src/app/component/navbar/navbar.component.ts
+++ frontend/src/app/component/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ReloadService } from '../../services/reload.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +9,30 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  userInfo: any;
+  img!: SafeUrl;
+  isVisible: boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private sanitizer: DomSanitizer,
+    private reloadService: ReloadService
+    ) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      if (this.isUserAuthenticated()) {
+        this.loadUserInfo();
+      }
+    }, 200)
+  }
+
+  loadUserInfo(): void {
+    this.userInfo = JSON.parse(localStorage.getItem('user_info')!);
+    this.img = this.sanitizer.bypassSecurityTrustUrl(this.userInfo.picture);
+    this.isVisible = true;
+  }
 
   isUserAuthenticated(): boolean {
     return localStorage.getItem('user_info') !== null;
